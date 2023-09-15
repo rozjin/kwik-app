7dcfefa0ae0fb09d65f92092f9d87db74be6ea2d
racemus <yavuz.rao@gmail.com>
2023-09-15 16:13:58 -0400

    offline check

--- app/providers.tsx
+++ app/providers.tsx
@@ -3,12 +3,32 @@
 import { NextUIProvider } from '@nextui-org/react';
 import { useEffect } from 'react';
 
+const timeoutFetch = async (resource: RequestInfo | URL, options: { timeout?: number } = {}) => {
+  const { timeout = 8000 } = options;
+  
+  const controller = new AbortController();
+  const id = setTimeout(() => controller.abort(), timeout);
+
+  const response = await fetch(resource, {
+    ...options,
+    signal: controller.signal  
+  });
+  clearTimeout(id);
+
+  return response;
+}
+
 const Providers = ({ children }: { children: React.ReactNode }) => {
   useEffect(() => {
     const waitForAPI = async () => {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/check`);
-      if (!res.ok) {
-        throw new Error("Kwik is offline.");
+      try {
+        const res = await timeoutFetch(`${process.env.NEXT_PUBLIC_API_URL}/check`);
+        if (!res.ok) {
+          throw new Error("Kwik is experiencing internal errors.");
+        }  
+      } catch (err: unknown) {
+        if (err instanceof Error) throw err;
+        throw new Error("Kwik is offline");
       }
     }
 
